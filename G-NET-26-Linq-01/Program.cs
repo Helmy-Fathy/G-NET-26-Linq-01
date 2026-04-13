f5aef911266955bcc7a1c327553b6593f90d9220
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-13 16:30:24 +0200

    2.Get a list of only the product names from ProductList

--- G-NET-26-Linq-01/Program.cs
+++ G-NET-26-Linq-01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LINQ.Models;
+using System.Xml.Linq;
 using static LINQ.DataSources.Source;
 
 namespace G_NET_26_Linq_01
@@ -7,13 +8,22 @@ namespace G_NET_26_Linq_01
     {
         static void Main(string[] args)
         {
-            #region Get all products from the "Seafood" category
-            //1. Get all products from the "Seafood" category. Print each product's name and price.
-            var seafoodProducts = ProductList.Where(p => p.Category == "Seafood");
-            foreach (var product in seafoodProducts)
-            {
-                Console.WriteLine($"Product's name: {product.ProductName}, Price: {product.UnitPrice}");
-            } 
+            #region 1.Get all products from the "Seafood" category
+            ////1. Get all products from the "Seafood" category. Print each product's name and price.
+            //var seafoodProducts = ProductList.Where(p => p.Category == "Seafood");
+            //foreach (var product in seafoodProducts)
+            //{
+            //    Console.WriteLine($"Product's name: {product.ProductName}, Price: {product.UnitPrice}");
+            //}
+            #endregion
+
+            #region 2.Get a list of only the product names from ProductList
+            ////2.Get a list of only the product names from ProductList.Print each name.
+            //var productNames = ProductList.Select(p => p.ProductName).ToList();
+            //foreach (var pName in productNames)
+            //{
+            //    Console.WriteLine(pName);
+            //}
             #endregion
 
         }
