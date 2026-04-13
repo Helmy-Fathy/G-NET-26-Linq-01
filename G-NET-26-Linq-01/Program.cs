212aa93adbb4f070f0025ffe2ebc3a179f24cdf1
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-13 16:25:21 +0200

    1. Get all products from the "Seafood" category.

--- G-NET-26-Linq-01/Program.cs
+++ G-NET-26-Linq-01/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace G_NET_26_Linq_01
+﻿using LINQ.Models;
+using static LINQ.DataSources.Source;
+
+namespace G_NET_26_Linq_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Get all products from the "Seafood" category
+            //1. Get all products from the "Seafood" category. Print each product's name and price.
+            var seafoodProducts = ProductList.Where(p => p.Category == "Seafood");
+            foreach (var product in seafoodProducts)
+            {
+                Console.WriteLine($"Product's name: {product.ProductName}, Price: {product.UnitPrice}");
+            } 
+            #endregion
+
         }
     }
 }
